a4f18ce9c21cdc31ad7de0f9376e81a5578c9758
Laroy Prado <laroyprado@gmail.com>
2023-07-19 18:31:00 -0300

    Funcionalidade de Upload de imagem 100% funcional

--- src/server.js
+++ src/server.js
@@ -3,15 +3,18 @@ require("express-async-errors");
 const migrationsRun = require("./database/sqlite/migrations");
 const AppError = require("./utills/AppError");
 const express = require("express");
+const uploadConfig = require("./configs/upload");
+const cors = require("cors")
 
 const routes = require("./routes");
 migrationsRun();
 
 const app = express();
+app.use(cors())
 app.use(express.json());
 
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
 app.use(routes);
-
 
 app.use((error, request, response, next) => {
   if (error instanceof AppError) {
